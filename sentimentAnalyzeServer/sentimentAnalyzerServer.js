61889b2d82fa1ccb7616ec3c22af912d1dc0a828
joanamdsantos <joanamlmsantos@gmail.com>
2021-05-31 15:35:22 +0000

    feat: Server Credentials

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,7 +1,26 @@
 const express = require('express');
-const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(express.static('client'))
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2020-08-01',
+    authenticator: new IamAuthenticator({
+        apikey: api_key,
+    }),
+    serviceUrl: api_url,
+    });
+    return naturalLanguageUnderstanding;
+}
+
+const app = new express();
+app.use(express.static('client'));
 
 const cors_app = require('cors');
 app.use(cors_app());
@@ -29,5 +48,4 @@ app.get("/text/sentiment", (req,res) => {
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
-})
-
+});
